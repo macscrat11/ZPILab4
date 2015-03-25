6d601409885811e7f0848683552c3b4b603db3d2
Michał Jabłoński <michal.jablonski1992@gmail.com>
2015-03-25 12:26:00 +0100

    dodanie main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+//Copyright by Michal Jablonski
+using namespace std;
+/// @brief Ca³y program, który niewiele co robi ...
+/// @return zawsze zwraca wartoœæ 0.
+int main()
+{
+ cout<<"Witaj WIOSNO :D!";
+ return 0;   
+}
